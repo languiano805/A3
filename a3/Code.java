fdefd3ddbed4067d3d29070b154caa5def406fb0
Anguinao, Leonardo <languiano@csus.edu>
2023-04-01 16:56:31 -0700

    used a simpler base file
    simpler base file, previous one was to complex to start with

--- a3/Code.java
+++ a3/Code.java
@@ -1,209 +1,149 @@
 package a3;
 
 import java.nio.*;
-import javax.swing.*;
 import java.lang.Math;
+import javax.swing.*;
 import static com.jogamp.opengl.GL4.*;
 import com.jogamp.opengl.*;
-import com.jogamp.opengl.util.*;
 import com.jogamp.opengl.awt.GLCanvas;
 import com.jogamp.common.nio.Buffers;
+import com.jogamp.opengl.GLContext;
 import org.joml.*;
 
 public class Code extends JFrame implements GLEventListener
 {	private GLCanvas myCanvas;
 	private int renderingProgram;
 	private int vao[] = new int[1];
-	private int vbo[] = new int[4];
-	private Vector3f torusLoc = new Vector3f(0,0,-1);
-	private Vector3f cameraLoc = new Vector3f(0,0,1);
-
-	private Torus myTorus;
-	private int numTorusVertices, numTorusIndices;
-
-	private Vector3f initialLightLoc = new Vector3f(5.0f, 2.0f, 2.0f);
-	private float amt = 0.0f;
-	private double prevTime;
-	private double elapsedTime;
+	private int vbo[] = new int[2];
+	private float cameraX, cameraY, cameraZ;
+	private float cubeLocX, cubeLocY, cubeLocZ;
+	private float pyrLocX, pyrLocY, pyrLocZ;
 	
 	// allocate variables for display() function
 	private FloatBuffer vals = Buffers.newDirectFloatBuffer(16);
 	private Matrix4f pMat = new Matrix4f();  // perspective matrix
 	private Matrix4f vMat = new Matrix4f();  // view matrix
 	private Matrix4f mMat = new Matrix4f();  // model matrix
-	private Matrix4f invTrMat = new Matrix4f(); // inverse-transpose
-	private int mLoc, vLoc, pLoc, nLoc;
-	private int globalAmbLoc, ambLoc, diffLoc, specLoc, posLoc, mambLoc, mdiffLoc, mspecLoc, mshiLoc;
+	private Matrix4f mvMat = new Matrix4f(); // model-view matrix
+	private int mvLoc, pLoc;
 	private float aspect;
-	private Vector3f currentLightPos = new Vector3f();
-	private float[] lightPos = new float[3];
-
-	// white light properties
-	float[] globalAmbient = new float[] { 0.6f, 0.6f, 0.6f, 1.0f };
-	float[] lightAmbient = new float[] { 0.1f, 0.1f, 0.1f, 1.0f };
-	float[] lightDiffuse = new float[] { 1.0f, 1.0f, 1.0f, 1.0f };
-	float[] lightSpecular = new float[] { 1.0f, 1.0f, 1.0f, 1.0f };
-		
-	// gold material
-	float[] matAmb = Utils.goldAmbient();
-	float[] matDif = Utils.goldDiffuse();
-	float[] matSpe = Utils.goldSpecular();
-	float matShi = Utils.goldShininess();
 
 	public Code()
-	{	setTitle("Chapter7 - program 3");
-		setSize(800, 800);
+	{	setTitle("Chapter 4 - program 3");
+		setSize(600, 600);
 		GLProfile glp = GLProfile.getMaxProgrammableCore(true);
 		GLCapabilities caps = new GLCapabilities(glp);
 		myCanvas = new GLCanvas(caps);
 		myCanvas.addGLEventListener(this);
 		this.add(myCanvas);
 		this.setVisible(true);
-		Animator animator = new Animator(myCanvas);
-		animator.start();
 	}
 
 	public void display(GLAutoDrawable drawable)
 	{	GL4 gl = (GL4) GLContext.getCurrentGL();
 		gl.glClear(GL_COLOR_BUFFER_BIT);
 		gl.glClear(GL_DEPTH_BUFFER_BIT);
+
 		gl.glUseProgram(renderingProgram);
 
-		mLoc = gl.glGetUniformLocation(renderingProgram, "m_matrix");
-		vLoc = gl.glGetUniformLocation(renderingProgram, "v_matrix");
+		mvLoc = gl.glGetUniformLocation(renderingProgram, "mv_matrix");
 		pLoc = gl.glGetUniformLocation(renderingProgram, "p_matrix");
-		nLoc = gl.glGetUniformLocation(renderingProgram, "norm_matrix");
-		
-		vMat.translation(-cameraLoc.x(), -cameraLoc.y(), -cameraLoc.z());
 
-		mMat.translation(torusLoc.x(), torusLoc.y(), torusLoc.z());
-		mMat.rotateX((float)Math.toRadians(35.0f));
-		
-		currentLightPos.set(initialLightLoc);
-		elapsedTime = System.currentTimeMillis() - prevTime;
-		prevTime = System.currentTimeMillis();
-		amt += elapsedTime * 0.03f;
-		currentLightPos.rotateAxis((float)Math.toRadians(amt), 0.0f, 0.0f, 1.0f);
-		
-		installLights();
+		aspect = (float) myCanvas.getWidth() / (float) myCanvas.getHeight();
+		pMat.setPerspective((float) Math.toRadians(60.0f), aspect, 0.1f, 1000.0f);
 
-		mMat.invert(invTrMat);
-		invTrMat.transpose(invTrMat);
+		vMat.translation(-cameraX,-cameraY,-cameraZ);
 
-		gl.glUniformMatrix4fv(mLoc, 1, false, mMat.get(vals));
-		gl.glUniformMatrix4fv(vLoc, 1, false, vMat.get(vals));
+		// draw the cube using buffer #0
+
+		mMat.translation(cubeLocX, cubeLocY, cubeLocZ);
+
+		mvMat.identity();
+		mvMat.mul(vMat);
+		mvMat.mul(mMat);
+
+		gl.glUniformMatrix4fv(mvLoc, 1, false, mvMat.get(vals));
 		gl.glUniformMatrix4fv(pLoc, 1, false, pMat.get(vals));
-		gl.glUniformMatrix4fv(nLoc, 1, false, invTrMat.get(vals));
 
 		gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 		gl.glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
 		gl.glEnableVertexAttribArray(0);
 
-		gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[2]);
-		gl.glVertexAttribPointer(1, 3, GL_FLOAT, false, 0, 0);
-		gl.glEnableVertexAttribArray(1);
-
-		gl.glEnable(GL_CULL_FACE);
-		gl.glFrontFace(GL_CCW);
 		gl.glEnable(GL_DEPTH_TEST);
 		gl.glDepthFunc(GL_LEQUAL);
 
-		gl.glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo[3]);
-		gl.glDrawElements(GL_TRIANGLES, numTorusIndices, GL_UNSIGNED_INT, 0);
+		gl.glDrawArrays(GL_TRIANGLES, 0, 36);
+		
+		// draw the pyramid using buffer #1
+
+		mMat.translation(pyrLocX, pyrLocY, pyrLocZ);
+
+		mvMat.identity();
+		mvMat.mul(vMat);
+		mvMat.mul(mMat);
+
+		gl.glUniformMatrix4fv(mvLoc, 1, false, mvMat.get(vals));
+		gl.glUniformMatrix4fv(pLoc, 1, false, pMat.get(vals));
+
+		gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+		gl.glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+		gl.glEnableVertexAttribArray(0);
+
+		gl.glEnable(GL_DEPTH_TEST);
+		gl.glDepthFunc(GL_LEQUAL);
+
+		gl.glDrawArrays(GL_TRIANGLES, 0, 18);
 	}
 
 	public void init(GLAutoDrawable drawable)
 	{	GL4 gl = (GL4) GLContext.getCurrentGL();
 		renderingProgram = Utils.createShaderProgram("a3/vertShader.glsl", "a3/fragShader.glsl");
-
-		prevTime = System.currentTimeMillis();
-
-		aspect = (float) myCanvas.getWidth() / (float) myCanvas.getHeight();
-		pMat.setPerspective((float) Math.toRadians(60.0f), aspect, 0.1f, 1000.0f);
-
 		setupVertices();
-	}
-	
-	private void installLights()
-	{	GL4 gl = (GL4) GLContext.getCurrentGL();
-		
-		lightPos[0]=currentLightPos.x(); lightPos[1]=currentLightPos.y(); lightPos[2]=currentLightPos.z();
-		
-		// get the locations of the light and material fields in the shader
-		globalAmbLoc = gl.glGetUniformLocation(renderingProgram, "globalAmbient");
-		ambLoc = gl.glGetUniformLocation(renderingProgram, "light.ambient");
-		diffLoc = gl.glGetUniformLocation(renderingProgram, "light.diffuse");
-		specLoc = gl.glGetUniformLocation(renderingProgram, "light.specular");
-		posLoc = gl.glGetUniformLocation(renderingProgram, "light.position");
-		mambLoc = gl.glGetUniformLocation(renderingProgram, "material.ambient");
-		mdiffLoc = gl.glGetUniformLocation(renderingProgram, "material.diffuse");
-		mspecLoc = gl.glGetUniformLocation(renderingProgram, "material.specular");
-		mshiLoc = gl.glGetUniformLocation(renderingProgram, "material.shininess");
-	
-		//  set the uniform light and material values in the shader
-		gl.glProgramUniform4fv(renderingProgram, globalAmbLoc, 1, globalAmbient, 0);
-		gl.glProgramUniform4fv(renderingProgram, ambLoc, 1, lightAmbient, 0);
-		gl.glProgramUniform4fv(renderingProgram, diffLoc, 1, lightDiffuse, 0);
-		gl.glProgramUniform4fv(renderingProgram, specLoc, 1, lightSpecular, 0);
-		gl.glProgramUniform3fv(renderingProgram, posLoc, 1, lightPos, 0);
-		gl.glProgramUniform4fv(renderingProgram, mambLoc, 1, matAmb, 0);
-		gl.glProgramUniform4fv(renderingProgram, mdiffLoc, 1, matDif, 0);
-		gl.glProgramUniform4fv(renderingProgram, mspecLoc, 1, matSpe, 0);
-		gl.glProgramUniform1f(renderingProgram, mshiLoc, matShi);
+		cameraX = 0.0f; cameraY = 0.0f; cameraZ = 8.0f;
+		cubeLocX = 0.0f; cubeLocY = -2.0f; cubeLocZ = 0.0f;
+		pyrLocX = 2.0f; pyrLocY = 2.0f; pyrLocZ = 0.0f;
 	}
 
 	private void setupVertices()
 	{	GL4 gl = (GL4) GLContext.getCurrentGL();
-	
-		myTorus = new Torus(0.5f, 0.2f, 48);
-		numTorusVertices = myTorus.getNumVertices();
-		numTorusIndices = myTorus.getNumIndices();
-	
-		Vector3f[] vertices = myTorus.getVertices();
-		Vector2f[] texCoords = myTorus.getTexCoords();
-		Vector3f[] normals = myTorus.getNormals();
-		int[] indices = myTorus.getIndices();
+		float[] cubePositions =
+		{	-1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f,
+			1.0f, -1.0f, -1.0f, 1.0f,  1.0f, -1.0f, -1.0f,  1.0f, -1.0f,
+			1.0f, -1.0f, -1.0f, 1.0f, -1.0f,  1.0f, 1.0f,  1.0f, -1.0f,
+			1.0f, -1.0f,  1.0f, 1.0f,  1.0f,  1.0f, 1.0f,  1.0f, -1.0f,
+			1.0f, -1.0f,  1.0f, -1.0f, -1.0f,  1.0f, 1.0f,  1.0f,  1.0f,
+			-1.0f, -1.0f,  1.0f, -1.0f,  1.0f,  1.0f, 1.0f,  1.0f,  1.0f,
+			-1.0f, -1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f,  1.0f,  1.0f,
+			-1.0f, -1.0f, -1.0f, -1.0f,  1.0f, -1.0f, -1.0f,  1.0f,  1.0f,
+			-1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f, -1.0f,
+			1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f,  1.0f,
+			-1.0f,  1.0f, -1.0f, 1.0f,  1.0f, -1.0f, 1.0f,  1.0f,  1.0f,
+			1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f, -1.0f
+		};
 		
-		float[] pvalues = new float[vertices.length*3];
-		float[] tvalues = new float[texCoords.length*2];
-		float[] nvalues = new float[normals.length*3];
+		float[] pyramidPositions =
+		{	-1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 0.0f, 1.0f, 0.0f,    //front
+			1.0f, -1.0f, 1.0f, 1.0f, -1.0f, -1.0f, 0.0f, 1.0f, 0.0f,    //right
+			1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 0.0f, 1.0f, 0.0f,  //back
+			-1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, 0.0f, 1.0f, 0.0f,  //left
+			-1.0f, -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, //LF
+			1.0f, -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f  //RR
+		};
 
-		for (int i=0; i<numTorusVertices; i++)
-		{	pvalues[i*3]   = (float) vertices[i].x();
-			pvalues[i*3+1] = (float) vertices[i].y();
-			pvalues[i*3+2] = (float) vertices[i].z();
-			tvalues[i*2]   = (float) texCoords[i].x();
-			tvalues[i*2+1] = (float) texCoords[i].y();
-			nvalues[i*3]   = (float) normals[i].x();
-			nvalues[i*3+1] = (float) normals[i].y();
-			nvalues[i*3+2] = (float) normals[i].z();
-		}
-		
 		gl.glGenVertexArrays(vao.length, vao, 0);
 		gl.glBindVertexArray(vao[0]);
-		gl.glGenBuffers(4, vbo, 0);
-		
+		gl.glGenBuffers(vbo.length, vbo, 0);
+
 		gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-		FloatBuffer vertBuf = Buffers.newDirectFloatBuffer(pvalues);
-		gl.glBufferData(GL_ARRAY_BUFFER, vertBuf.limit()*4, vertBuf, GL_STATIC_DRAW);
-
-		gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
-		FloatBuffer texBuf = Buffers.newDirectFloatBuffer(tvalues);
-		gl.glBufferData(GL_ARRAY_BUFFER, texBuf.limit()*4, texBuf, GL_STATIC_DRAW);
-
-		gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[2]);
-		FloatBuffer norBuf = Buffers.newDirectFloatBuffer(nvalues);
-		gl.glBufferData(GL_ARRAY_BUFFER, norBuf.limit()*4, norBuf, GL_STATIC_DRAW);
+		FloatBuffer cubeBuf = Buffers.newDirectFloatBuffer(cubePositions);
+		gl.glBufferData(GL_ARRAY_BUFFER, cubeBuf.limit()*4, cubeBuf, GL_STATIC_DRAW);
 		
-		gl.glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo[3]);
-		IntBuffer idxBuf = Buffers.newDirectIntBuffer(indices);
-		gl.glBufferData(GL_ELEMENT_ARRAY_BUFFER, idxBuf.limit()*4, idxBuf, GL_STATIC_DRAW);
+		gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+		FloatBuffer pyrBuf = Buffers.newDirectFloatBuffer(pyramidPositions);
+		gl.glBufferData(GL_ARRAY_BUFFER, pyrBuf.limit()*4, pyrBuf, GL_STATIC_DRAW);
 	}
 
 	public static void main(String[] args) { new Code(); }
+	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {}
 	public void dispose(GLAutoDrawable drawable) {}
-	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height)
-	{	aspect = (float) myCanvas.getWidth() / (float) myCanvas.getHeight();
-		pMat.setPerspective((float) Math.toRadians(60.0f), aspect, 0.1f, 1000.0f);
-	}
 }
--- a3/vertShader.glsl
+++ a3/vertShader.glsl
@@ -1,41 +1,15 @@
 #version 410
 
-layout (location = 0) in vec3 vertPos;
-layout (location = 1) in vec3 vertNormal;
-out vec3 varyingNormal;
-out vec3 varyingLightDir;
-out vec3 varyingVertPos;
-out vec3 varyingHalfVector;
+layout (location=0) in vec3 position;
 
-struct PositionalLight
-{	vec4 ambient;
-	vec4 diffuse;
-	vec4 specular;
-	vec3 position;
-};
-struct Material
-{	vec4 ambient;
-	vec4 diffuse;
-	vec4 specular;
-	float shininess;
-};
-
-uniform vec4 globalAmbient;
-uniform PositionalLight light;
-uniform Material material;
-uniform mat4 m_matrix;
-uniform mat4 v_matrix;
+uniform mat4 mv_matrix;
 uniform mat4 p_matrix;
-uniform mat4 norm_matrix;
+
+out vec4 varyingColor;
 
 void main(void)
-{	varyingVertPos = (m_matrix * vec4(vertPos,1.0)).xyz;
-	varyingLightDir = light.position - varyingVertPos;
-	varyingNormal = (norm_matrix * vec4(vertNormal,1.0)).xyz;
+{
+	gl_Position = p_matrix * mv_matrix * vec4(position,1.0);
 	
-	varyingHalfVector =
-		normalize(normalize(varyingLightDir)
-		+ normalize(-varyingVertPos)).xyz;
-
-	gl_Position = p_matrix * v_matrix * m_matrix * vec4(vertPos,1.0);
-}
+	varyingColor = vec4(position,1.0)*0.5 + vec4(0.5, 0.5, 0.5, 0.5);
+} 
